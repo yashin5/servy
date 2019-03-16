4e2736d930ca9d5915a833e4573a00e5fab6ec4e
Yashin Santos <31665100+yashin5@users.noreply.github.com>
2019-03-16 11:53:04 -0300

    follow course

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,13 +1,39 @@
 defmodule Servy.Handler do
-  @spec handle(any()) :: nil
+  @moduledoc "Hadles HTTP request."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transform the request into a response."
+  @spec handle(binary()) :: <<_::64, _::_*8>>
   def handle(request) do
     request
     |> parse()
+    |> rewrite_path()
+    |> log()
     |> route()
+    |> track()
     |> format_response()
   end
 
-  @spec parse(any()) :: %{method: <<_::24>>, path: <<_::88>>, resp_body: <<>>}
+  @spec track(any()) :: any()
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts("Warning: #{path} is on the loose!")
+    conv
+  end
+
+  def track(conv), do: conv
+
+  @spec rewrite_path(any()) :: any()
+  def rewrite_path(%{path: "/wilflife"} = conv) do
+    %{conv | path: "wildthings"}
+  end
+
+  def rewrite_path(conv), do: conv
+
+  @spec log(any()) :: any()
+  def log(conv), do: IO.inspect(conv)
+
+  @spec parse(binary()) :: %{method: binary(), path: binary(), resp_body: <<>>, status: nil}
   def parse(request) do
     [method, path, _] =
       request
@@ -15,57 +41,126 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
-  @spec route(any()) :: %{method: <<_::24>>, path: <<_::88>>, resp_body: <<_::160>>}
-  def route(conv) do
-    route(conv, conv.method, conv.path)
+  @spec route(%{path: any(), resp_body: any(), status: any()}) :: %{
+          path: any(),
+          resp_body: <<_::40, _::_*8>>,
+          status: 200 | 404
+        }
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(conv, "GET", "/wildthings") do
-    %{conv | resp_body: "Bears, Lions, Tigers"}
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(conv, "GET", "/bearanimal") do
-    %{conv | resp_body: "Teddy, Loly, Tiny"}
+  def route(%{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
-  @spec format_response(any()) :: <<_::712>>
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(%{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  @spec handle_file({:error, any()} | {:ok, any()}, %{resp_body: any(), status: any()}) :: %{
+          resp_body: any(),
+          status: 200 | 404
+        }
+  def handle_file({:ok, content}, conv) do
+    %{conv | status: 200, resp_body: content}
+  end
+
+  def handle_file({:error, _enoent}, conv) do
+    %{conv | status: 404, resp_body: "File not found!"}
+  end
+
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
+  end
+
+  @spec format_response(atom() | %{resp_body: binary(), status: any()}) :: <<_::64, _::_*8>>
   def format_response(conv) do
     """
-      HTTP/1.1 200 OK
-      Content-Type: text/html
-      Content-Length: #{String.length(conv.resp_body)}
-
-      #{conv.resp_body}
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+    #{conv.resp_body}
     """
   end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
+
 request = """
 GET /wildthings HTTP/1.1
 Host: example.com
-User-Agente: ExampleBrowser/1.0
+User-Agent: ExampleBrowser/1.0
 Accept: */*
-
 """
-
-
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
 
 request = """
-GET /bearanimal HTTP/1.1
+GET /bears HTTP/1.1
 Host: example.com
-User-Agente: ExampleBrowser/1.0
+User-Agent: ExampleBrowser/1.0
 Accept: */*
-
 """
-
-
 
 response = Servy.Handler.handle(request)
 
-IO.puts response
+IO.puts(response)
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
